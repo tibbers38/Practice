f144966cfb78cabfdf3feebef1f597b2522d907e
LH Nam <lehainam38@gmail.com>
2017-09-15 22:54:43 +0700

    Initial Commit

---
+++ Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Practice
+//
+//  Created by Nam 😋 on 9/15/17.
+//  Copyright © 2017 Nam 😋. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
